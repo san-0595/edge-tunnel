e449ee21cd8fefbc835b85df92e947a694233d77
ImXiaoYe <2423336405@qq.com>
2025-03-19 19:35:00 +0800

    1

--- 明文源码.js
+++ 明文源码.js
@@ -433,28 +433,10 @@ ${代理配置}
   proxies:
 ${代理配置}
 
-rule-providers:
-  reject-domain:
-    type: http
-    behavior: domain
-    url: "https://raw.githubusercontent.com/ImLTHQ/SpotifyAdBlock/master/reject-domain.list"
-    path: ./ruleset/reject-domain.yaml
-    interval: 86400
-
-  reject-ip:
-    type: http
-    behavior: ipcidr
-    url: "https://raw.githubusercontent.com/ImLTHQ/SpotifyAdBlock/master/reject-ip.list"
-    path: ./ruleset/reject-ip.yaml
-    interval: 86400
-
-geodata-mode: true
-
 rules:
-  - GEOIP,CN,DIRECT
-  - GEOSITE,CN,DIRECT
+  - GEOIP,CN,DIRECT,no-resolve
+  - RULE-SET,reject-ip,REJECT,no-resolve
   - RULE-SET,reject-domain,REJECT
-  - RULE-SET,reject-ip,REJECT
   - MATCH,🚀 节点选择
 `;
 
